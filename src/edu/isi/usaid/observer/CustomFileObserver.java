de6556e03c26cc23b0302f0cca22a67112ad48ce
Shanthoosh <spvenkat@isi.edu>
2013-10-15 13:32:04 -0700

    Adding metadata file merge for articles.

--- src/edu/isi/usaid/observer/CustomFileObserver.java
+++ src/edu/isi/usaid/observer/CustomFileObserver.java
@@ -11,6 +11,8 @@ import java.util.List;
 import org.apache.commons.io.FileUtils;
 
 import edu.isi.usaid.pifi.Constants;
+import edu.isi.usaid.pifi.metadata.ArticleProtos.Article;
+import edu.isi.usaid.pifi.metadata.ArticleProtos.Articles;
 import edu.isi.usaid.pifi.metadata.VideoProtos.Video;
 import edu.isi.usaid.pifi.metadata.VideoProtos.Videos;
 import edu.isi.usaid.pifi.metadata.VideoProtos.Videos.Builder;
@@ -21,6 +23,8 @@ import android.util.Log;
 
 public class CustomFileObserver extends FileObserver
 {
+	public static final String ARTICLE_META_FILE_LOCATION = Constants.webMetaFileName;
+
 	public static final String VIDEO_META_FILE_LOCATION = Constants.metaFileName;
 
 	private static String tagName = "CustomFileObserver";
@@ -36,8 +40,8 @@ public class CustomFileObserver extends FileObserver
 	{
 		return path != null && path.contains("transfer");
 	}
-	@Override
 	
+	@Override
 	public void onEvent(int event, String path) 
 	{
 		Log.d(tagName, "Got event for file with path: " + path);
@@ -78,15 +82,33 @@ public class CustomFileObserver extends FileObserver
 		} 
 		catch (FileNotFoundException e) 
 		{
-			Log.e(tagName, "Exception in merging metadatafile. No File exists at: "   
-									  + fileName, e.getCause());
+			Log.e(tagName, "No File exists at: " + fileName, e.getCause());
 			return new ArrayList<Video>();
 		} 
 		catch (IOException e) 
 		{
-			Log.e(tagName, "Exception in merging metadatafile. Exception while reading file at: "   
+			Log.e(tagName, "Exception in reading metadatafile. Exception while reading file at: "   
 					  				   + fileName, e.getCause());
 			return new ArrayList<Video>();
+		}
+	}
+	
+	private List<Article> getArticlesFromMetaFile(String fileName)
+	{
+		try 
+		{
+			Articles articles = Articles.parseFrom(new FileInputStream(fileName));
+			return articles.getArticleList();
+		} 
+		catch (FileNotFoundException e) 
+		{
+			Log.e(tagName, "No File exists at: " + fileName, e.getCause());
+			return new ArrayList<Article>();
+		} 
+		catch (IOException e) {
+			Log.e(tagName, "Exception in reading metadatafile. Exception while reading file at: "   
+	  				   + fileName, e.getCause());
+			return new ArrayList<Article>();
 		}
 	}
 	
@@ -94,13 +116,13 @@ public class CustomFileObserver extends FileObserver
 	{
 		List<Video> newMetaFileVideos = getVideosFromFile(sourceMetaFilePath);
 		List<Video> existingMetaFileVideos = getVideosFromFile(VIDEO_META_FILE_LOCATION);
-		if(newMetaFileVideos.isEmpty() || existingMetaFileVideos.isEmpty()) 
+		if(newMetaFileVideos.isEmpty()) 
 			return;
 		existingMetaFileVideos.addAll(newMetaFileVideos);
-		writeToOutputFile(existingMetaFileVideos);
+		writeUpdatedVideos(existingMetaFileVideos);
 	}
 
-	private void writeToOutputFile(List<Video> videoList) 
+	private void writeUpdatedVideos(List<Video> videoList) 
 	{
 		Builder videoBuilder = Videos.newBuilder();
 		videoBuilder.addAllVideo(videoList);
@@ -124,7 +146,33 @@ public class CustomFileObserver extends FileObserver
 
 	private void appendToNewsMetaFile(String sourceMetaFilePath) 
 	{
-		
+		List<Article> newArticles = getArticlesFromMetaFile(sourceMetaFilePath);
+		List<Article> existingArticles = getArticlesFromMetaFile(ARTICLE_META_FILE_LOCATION);
+		if(newArticles.isEmpty())
+			return;
+		existingArticles.addAll(newArticles);
+		writeUpdatedArticles(existingArticles);
+	}
+
+	private void writeUpdatedArticles(List<Article> existingArticles) 
+	{
+		edu.isi.usaid.pifi.metadata.ArticleProtos.Articles.Builder articleBuilder = Articles.newBuilder();
+		articleBuilder.addAllArticle(existingArticles);
+		Articles appendedArticles = articleBuilder.build();
+		try 
+		{
+			appendedArticles.writeTo(new FileOutputStream(ARTICLE_META_FILE_LOCATION));
+		} 
+		catch (FileNotFoundException e) 
+		{
+			Log.e(tagName, "FileNotFoundException occured for the article meta data file "
+						   + " at location: " + VIDEO_META_FILE_LOCATION, e.getCause());
+		} 
+		catch (IOException e) 
+		{
+			Log.e(tagName, "IOException occured when writing article meta data file "
+					   + " at location: " + VIDEO_META_FILE_LOCATION, e.getCause());
+		}
 	}
 
 	private boolean isMetaDataFile(String path) 
